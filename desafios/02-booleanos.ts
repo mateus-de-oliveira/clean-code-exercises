37f70001a6fad229ae235c0716b3901a6cb25a4a
Mateus de Oliveira Ferreira <mateusdeoliveira1530@gmail.com>
2022-12-06 06:07:20 -0300

    refactor: making boolean naming more readable

--- desafios/02-booleanos.ts
+++ desafios/02-booleanos.ts
@@ -1,7 +1,7 @@
 // Boleanos
 
 const user = {
-  name: 'Diego Fernandes',
+  name: "Diego Fernandes",
   height: 190,
   hasTicket: true,
 }
@@ -10,22 +10,22 @@ const necessaryHeight = 130
 
 const currentHour = new Date().getHours()
 
-const parkOpen = currentHour > 9 && currentHour < 18
+const parkIsOpen = currentHour > 9 && currentHour < 18
 
-if (!parkOpen) {
-  throw new Error('O parque está fechado!')
+if (!parkIsOpen) {
+  throw new Error("O parque está fechado!")
 }
 
-const ticket = user.hasTicket
+const hasTicket = user.hasTicket
 
-if (!ticket) {
-  throw new Error('O Diego não possui um bilhete para entrar no parque!')
+if (!hasTicket) {
+  throw new Error("O Diego não possui um bilhete para entrar no parque!")
 }
 
-const enterToy = user.height > necessaryHeight
+const canEnterTheToy = user.height > necessaryHeight
 
-if (!enterToy) {
-  throw new Error('O Diego não pode entrar no brinquedo!')
-} 
+if (!canEnterTheToy) {
+  throw new Error("O Diego não pode entrar no brinquedo!")
+}
 
-console.log('O Diego se divertiu muito! :)')
+console.log("O Diego se divertiu muito! :)")
