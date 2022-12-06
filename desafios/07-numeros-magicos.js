698cbfeda7bdf3060816363a7b661c019ee30aee
Mateus de Oliveira Ferreira <mateusdeoliveira1530@gmail.com>
2022-12-06 07:05:11 -0300

    refactor: refactoring magic numbers on code

--- desafios/07-numeros-magicos.js
+++ desafios/07-numeros-magicos.js
@@ -1,5 +1,7 @@
 function lookForUpdades() {}
 
-setInterval(lookForUpdades, 60 * 30 * 1000)
+const INTERVAL_30_MINUTES = 60 * 30 * 1000
 
-function calculateDiscount(price, discount) {}
+setInterval(lookForUpdades, INTERVAL_30_MINUTES)
+
+function calculateDiscount(priceInCents, discountInPercent) {}
